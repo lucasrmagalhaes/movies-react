060be5837fad0d00b46f4a4f26524b13d35b2144
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-23 10:26:18 -0300

    Call the API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MovieList from './components/MovieList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-  const [movies, setMovies] = useState([        
-    {
-      "Title": "Star Wars: Episode VI - Return of the Jedi",
-      "Year": "1983",
-      "imdbID": "tt0086190",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Star Wars: Episode VII - The Force Awakens",
-      "Year": "2015",
-      "imdbID": "tt2488496",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  const getMovieRequest = async () => {
+    const url = "https://www.omdbapi.com/?s=avengers&apikey=263d22d8";
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    // console.log(responseJson);
+    setMovies(responseJson.Search);
+  }
+
+  useEffect(() => {
+    getMovieRequest();
+  }, [])
 
   return (
     <div className='container-fluid movie-app'>
