25d5f4b5d6f8047313292f22e53d4418b9575715
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-23 10:32:35 -0300

    Heading

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import MovieList from './components/MovieList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieListHeading from './components/MovieListHeading';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
     const url = "https://www.omdbapi.com/?s=avengers&apikey=263d22d8";
@@ -23,6 +25,9 @@ function App() {
 
   return (
     <div className='container-fluid movie-app'>
+      <div className='row'>
+        <MovieListHeading heading='Movies' />
+      </div>
       <div className='row'>
         <MovieList movies={movies} />
       </div>
---
+++ src/components/MovieListHeading.js
@@ -0,0 +1,9 @@
+const MovieListHeading = ({ heading }) => {
+  return (
+    <div className='col'>
+      <h1>{heading}</h1>
+    </div>
+  )
+}
+
+export default MovieListHeading
