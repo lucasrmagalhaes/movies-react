4b9e69a7a8ae0c5cc4d137fdf8fbf7e0ea6d45c3
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-23 11:04:38 -0300

    Hover Effect

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,8 +4,9 @@ const MovieList = (props) => {
   return (
     <>
       {props.movies.map((movie, index) => (
-        <div className='d-flex justify-content-start m-3'>
+        <div className='image-container d-flex justify-content-start m-4'>
           <img src={movie.Poster} alt='Movie' />
+          <div className='overlay d-flex align-items-center justify-content'></div>
         </div>
       ))}
     </>
